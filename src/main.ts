fb0b02252eb86b4e95b9d0499b040ca98818b5a3
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2016-08-15 13:16:54 +0300

    rc5 support

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,6 @@ import { bootstrap }    from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
 
 bootstrap(AppComponent);
+
+
+
--- systemjs.config.js
+++ systemjs.config.js
@@ -12,7 +12,7 @@
   };
   // packages tells the System loader how to load when no filename and/or no extension
   var packages = {
-    'app':                        { main: 'main.js',  defaultExtension: 'js' },
+    'app':                        { main: 'main.rc5.js',  defaultExtension: 'js' },
     'rxjs':                       { defaultExtension: 'js' },
     'angular2-in-memory-web-api': { main: 'index.js', defaultExtension: 'js' },
   };
