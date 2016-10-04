f6288b9bcfa08a409cbecf0e9b4d896ebbc4dc1a
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2016-10-04 10:54:54 +0300

    update to v.2.0.1

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrap(AppComponent);
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic()
+platform.bootstrapModule(AppModule);
 
 
 
