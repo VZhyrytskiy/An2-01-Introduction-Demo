2ac149d62b90705d6921868ab121a89824799308
Vitaliy Zhyrytskyy <vitaliy_zhyrytskyy@epam.com>
2016-11-18 10:55:35 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 const platform = platformBrowserDynamic()
-platform.bootstrapModule(AppModule);
+platform.bootstrapModule(AppModule)
+  .then(success => console.log('Bootstrap success'))
+  .catch(err => console.error(err));;
 
 
 
