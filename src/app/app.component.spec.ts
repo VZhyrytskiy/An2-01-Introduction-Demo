b9b08f0f940dbb71a14302f343a26e6f718edec3
upskill js.basics <upskill.js.basics@gmail.com>
2021-05-19 20:29:20 +0300

    upd" update to Angular v12

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'an'`, () => {
+  it(`should have as title 'an2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('an');
+    expect(app.title).toEqual('an2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('an app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('an2 app is running!');
   });
 });
